502119920cf7a2066dd7c9979460179065ba6b62
Stephen Brown II <Stephen.Brown2@gmail.com>
2015-04-13 16:20:08 -0500

    Hashing the filename, and formatting adjustments for paste.sh

--- pasted/paste.sh
+++ pasted/paste.sh
@@ -3,14 +3,15 @@ ulimit -c 0
 ulimit -f 1024
 ulimit -t 10
 ulimit -m 1024
-path=/home/paste
-file=$path"/out/out_"$REMOTE_HOST"_"$(date +%s )".html"
-cat $path"/pasted/"header > $file
+localpath=/home/paste
+outhash=$(sha256sum <<< "${REMOTE_HOST}_$(date +%s )" | cut -d' ' -f1)
+file="${localpath}/out/out_${outhash}.html"
+cat ${localpath}/pasted/header > $file
 echo http://graph.sanxiago.com/out/$(basename $file)
-  if read -t 0; 
-    then
-        cat | sed -s 's/</./g' >> $file 
-    else
-        echo "$*" | sed -s 's/</./g'  > $file
-    fi
-cat $path"/pasted/"footer >> $file
+if read -t 0;
+then
+    cat | sed -s 's/</./g' >> $file
+else
+    echo "$*" | sed -s 's/</./g' > $file
+fi
+cat ${localpath}/pasted/footer >> $file
