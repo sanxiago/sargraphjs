cc4b94563f7426f0d32a597e148862842b859b22
sanxiago <graph@sanxiago.com>
2015-04-13 23:28:26 +0000

    Included merges from Stephen

--- pasted/paste.sh
+++ pasted/paste.sh
@@ -3,18 +3,15 @@ ulimit -c 0
 ulimit -f 1024
 ulimit -t 10
 ulimit -m 1024
-path=/home/paste
-random=$(base64 /dev/urandom | head -c 32 )
-timestamp=$(date +%s)
-F=$(echo $random$timestamp | base64 | sed -s 's/\//P/g' | sed -s 's/=/F/g')
-file=$path"/out/"$F".html"
-
-cat $path"/pasted/"header > $file
+localpath=/home/paste
+outhash=$(sha256sum <<< "${REMOTE_HOST}_$(date +%s )" | cut -d' ' -f1)
+file="${localpath}/out/${outhash}.html"
+cat ${localpath}/pasted/header > $file
 echo http://graph.sanxiago.com/out/$(basename $file)
-  if read -t 0; 
-    then
-        cat | sed -s 's/</./g' >> $file 
-    else
-        echo "$*" | sed -s 's/</./g'  > $file
-    fi
-cat $path"/pasted/"footer >> $file
+if read -t 0;
+then
+    cat | sed -s 's/</./g' >> $file
+else
+    echo "$*" | sed -s 's/</./g' > $file
+fi
+cat ${localpath}/pasted/footer >> $file
