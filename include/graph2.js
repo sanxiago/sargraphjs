87dc266f7353565c7bba2abbe544e63ef098e35a
sanxiago <graph@sanxiago.com>
2015-05-05 17:51:20 +0000

    Completed parsing of sadf output and switched to c3d3 graphing library

--- include/graph2.js
+++ include/graph2.js
@@ -1,6 +1,4 @@
 
-google.load('visualization', '1', {packages: ['corechart', 'line']});
-//google.setOnLoadCallback(drawBasic);
 
 // human friendly titles based on the first line
 var graph_titles = { 
@@ -29,165 +27,80 @@ var graph_titles = {
 "NFS client activity" : "timestamp;call/s;retrans/s;read/s;write/s;access/s;getatt/s",
 "NFSD server activity" : "timestamp;scall/s;badcall/s;packet/s;udp/s;tcp/s;hit/s;miss/s;sread/s;swrite/s;saccess/s;sgetatt/s",
 "Network Sockets" : "timestamp;totsck;tcpsck;udpsck;rawsck;ip-frag;tcp-tw"
-};
-	
+}
+
+var datetimes = []
+var statistics = []
+    
 // nice to have a hash with the description of each element for reference
-function readLines(){
-     document.getElementById('output').innerHTML='';
-     text = document.getElementById('textinput').value;
-     var lines = text.split("\n");
-     for(var x in lines){
-        line = lines[x];
-        var m = line.match(/([0-9]{4})-([0-1][0-9])-([0-3][0-9])\s+([0-2][0-9])(:|-)([0-9][0-9])(:|-)([0-9][0-9])\s+([A-z]+)/);
-        if(m != null){ // Check if you find formated date and replace it with posix timestamp
-	   var unix = Math.round(+new Date(m[1],m[2]-1,m[3],m[4],m[6],m[8])/1000);
-           line=line.replace(m[0], unix);
-        }
-        var values = line.match(/(\w+)\s+(\d+)\s+(\d+)\s+(\w+)\s+([A-z%]+)\s+([0-9\.\,]+)/);
+function readInput(){
+    datetimes = {}
+    statistics = {}
+    document.getElementById('output').innerHTML=''
+    text = document.getElementById('textinput').value
+    var lines = text.split("\n")
+   for(var x in lines){
+        line = lines[x]
+        var values = line.match(/(\w+)\s+(\d+)\s+([A-z0-9:-\s]+)\s+([^\s]+)\s+([^\s]+)\s+([0-9\.\,-]+)/)
         if(values !=null){
-            values.shift(); // remove main match
-            console.log(values);
-            
+           // console.log(values)
+            var interval = values[2]
+            var time = values[3]
+            var stat = values[5]
+            var label = values[4]
+            var value = values[6]  
+
+            var mm = time.match(/^[0-9]+\s*$/)
+            if(mm != null){
+                time = new Date(time * 1000).toISOString()
+            }
+            var m = time.match(/([0-9]{4})-([0-1][0-9])-([0-3][0-9])[^0-9]+([0-2][0-9])(:|-)([0-9][0-9])(:|-)([0-9][0-9])\.*/)
+            if(m != null){ // Check if you find formated date and replace it with posix timestamp
+                    var formated = ''
+                    time =  m[1] + "-" + m[2] + "-" + m[3] + ":" + m[4] + ":" + m[6] + ":" + m[8] 
+            }
+ 
+            datetimes[time]=interval
+            if( statistics[stat] == undefined){
+                statistics[stat] = []
+            }
+            if( statistics[stat][label] == undefined ){
+                statistics[stat][label] = []
+            }
+            statistics[stat][label].push(value)
+        }
+        else{
+            if(line != ''){
+                console.log("ERROR: Cant Parse line "+line)
+            } 
         }
      } // foreach line
-}
-function readInput() {
-     readLines()
-     document.getElementById('output').innerHTML='';
-     text = document.getElementById('textinput').value;
-     sar_sections = text.split("# ");
-     sar_sections.splice(0,1);
-    var sar_headers = {} ;
-    for(var n in sar_sections){
-	createDiv(n,"output"); 
-        var patt = new RegExp("^[A-Z]+$"); // UPPER CASE means CPU IFACE DEV multichart
-        var is_multichart = false;
-	var headers = {};
-	var data = {};
-	var title = '';
-	var subcharts = {};
-       // var data = new google.visualization.DataTable();
-        var lines = sar_sections[n].split("\n");
-        lines.pop(); // trailing empty line
-        for(var x in lines){
-            var fields = lines[x].split(";");
-            if( x==0 ){
-		for(var t in graph_titles){
-			var is_title = new RegExp(graph_titles[t]);
-			if ( is_title.exec(lines[x]) != null ){
-				var title = t;
-			}
-		}
-		document.getElementById("title_"+n).innerHTML= '<h3>' + title + '</h3>';
-            	for(var y in fields){
-		   headers[y]=fields[y];
-			if( patt.exec(fields[y]) != null ){
-			        var chart_title = y;
-				is_multichart = true;
-			} // if header is upper case it is multi chart
-            } //foreach fields
-            } // if first line is header
-	    else{
-		if(is_multichart){
-			title=headers[chart_title]+"_"+fields[chart_title];
-			subcharts[title]=1;
-			if(data[title]==undefined){ data[title]=[]; }
-			data[title].push(fields);
-		}else{
-			title=lines[0];
-			if(data[title]==undefined){ data[title]=[]; }
-			data[title].push(fields);
-		}
-		}
-        } // foreach lines
-	if(is_multichart){
-	for(var title in subcharts){
-		createDiv(title,"output_"+n);
-		document.getElementById("title_"+title).innerHTML= '<h3>' + title + '</h3>';
-		graphData( headers , data[title],"graph_"+title);
-	}
-	}
-	else{	
-		console.log(title);
-		graphData(headers , data[title],"graph_"+n);
-	}
-    } // foreach sar_sections
-}// graph_data
-
-function createDiv(id,target) {
-	parentElement = document.getElementById(target);
-     	var d = document.createElement("div");
-	d.setAttribute("id", "output_"+id);
-     	parentElement.appendChild(d);
-     	parentElement = document.getElementById("output_"+id);
-       	var d = document.createElement("p");
-        d.setAttribute("id", "title_"+id);
-      	parentElement.appendChild(d);
-    	var d = document.createElement("div");
-      	d.setAttribute("id" , "graph_"+id);
-      	parentElement.appendChild(d);
+print()
 }
 
-function graphData(headers,datas,div){
-	var data = new google.visualization.DataTable();
-	var check_is_number = new RegExp("^[0-9\.]+$");
-	var check_is_timestamp = new RegExp("timestamp");
-	var check_is_excluded = new RegExp("^[A-Z]+$|hostname|interval");
-	var excluded= {};
-	var is_timestamp = {} ;
-	var rows = [];
-	for ( var header in headers){
-		if( check_is_timestamp.test(headers[header])){
-			excluded[header]=true;
-      			data.addColumn('datetime', "X");
-			is_timestamp[header]=true;
-		}
-		else{
-			if( check_is_excluded.test(headers[header])){
-				excluded[header]=true;
-			}else{
-				excluded[header]=false;
-      				data.addColumn('number', headers[header]);
-			}
-		}
-	}
-//	console.log(excluded);
-//	console.log(datas);
-	for( var i in datas){
-		var row = [];
-		for( var j in datas[i]){
-			if(excluded[j]==false){
-			//	console.log(i + "," + j + "->" + datas[i][j]);
-				row.push(Number(datas[i][j]));
-			}else { if(is_timestamp[j]){
-				var m = datas[i][j].match(/([0-9]{4})-([0-1][0-9])-([0-3][0-9])\s+([0-2][0-9])(:|-)([0-9][0-9])(:|-)([0-9][0-9])\s+([A-z]+)/);
-				if(m != null){
-					row.push(new Date(m[1],m[2]-1,m[3],m[4],m[6],m[8]));
-				}
-			}}
-		}
-		if(m != null){
-			rows.push(row);
-		}
-	}
-//	console.log(rows);
-//	console.log(data);
-	data.addRows(rows);
-
-      var options = {
-        hAxis: {
-          title: 'Time'
+function print(){
+    var col_x = []
+    col_x.push('date')
+    for (d in datetimes){
+        col_x.push(d)
+    }
+    console.log(statistics)
+    var chart_args = {
+        bindto: '#chart_div',
+        data: {
+          x : 'date',
+          xFormat : '%Y-%m-%d:%H:%M:%S',
+          columns: [col_x, statistics['%idle']['all']  ]
         },
-        vAxis: {
-          title: ''
-        },
-	legend: {
-		position: 'top',
-		maxLines: 12
-	}
-      };
-
-
-      var chart = new google.visualization.LineChart(document.getElementById(div));
-      chart.draw(data, options);
+        axis : {
+          x : {
+            type : 'timeseries',
+            tick : {
+                format : "%H:%M" // https://github.com/mbostock/d3/wiki/Time-Formatting#wiki-format
+            }
+          }
+        }
+      }
+    console.log(chart_args)
+    var chart = c3.generate(chart_args)
 }
