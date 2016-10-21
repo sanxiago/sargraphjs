4541f4c67a5abaa7df41e1e593c5620511bceaed
sanxiago <graph@sanxiago.com>
2016-10-21 11:13:58 +0000

    timeout plus direct

--- pasted/paste.sh
+++ pasted/paste.sh
@@ -4,13 +4,23 @@ ulimit -f 8000
 ulimit -t 300
 ulimit -m 8000
 localpath=/home/paste
-outhash=$(sha256sum <<< "${REMOTE_HOST}_$(date +%s )" | cut -d' ' -f1)
-file="${localpath}/out/${outhash}.html"
+cd ${localpath}
+random=$(base64 /dev/urandom | head -c 6  )
+STRING="${REMOTE_HOST} ${random}.html"
+# first, strip underscores
+CLEAN=${STRING//_/}
+# next, replace spaces with underscores
+CLEAN=${CLEAN// /_}
+# now, clean out anything that's not alphanumeric or an underscore
+CLEAN=${CLEAN//[^a-zA-Z0-9\_\.]/}
+# finally, lowercase with TR
+CLEAN=`echo -n $CLEAN | tr A-Z a-z`
+file=out/${CLEAN}
 cat ${localpath}/pasted/header > $file
-echo http://graph.sanxiago.com/out/${outhash}.html
+echo http://graph.sanxiago.com/${file}
 if read -t 0;
 then
-    cat | sed -s 's/</./g' >> $file
+    timeout 60s cat | sed -s 's/</./g' >> $file
 else
     echo "$*" | sed -s 's/</./g' > $file
 fi
